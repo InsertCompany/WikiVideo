764cd7edc955a0f45081b77638fbcc511a323f93
unknown <cleidesonbarbosa5@gmail.com>
2020-06-06 19:52:12 -0300

    Config path

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://wikivideo.ga/wikivideo';
+$config['base_url'] = 'https://wiki-video.herokuapp.com/';
 
 
 /*
